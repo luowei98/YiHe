f3374af8b26bf323676e72c414d0981cadc98caf
luowei98 <luowei98@hotmail.com>
2018-04-23 14:13:22 +0800

    long time update

---
+++ YiHe.Web/Controllers/TrainController.cs
@@ -0,0 +1,24 @@
+﻿using YiHe.CommandProcessor.Dispatcher;
+using YiHe.Data.Repositories.Material;
+using YiHe.Data.Repositories.Navigation;
+
+
+namespace YiHe.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class TrainController : ControllerBase
+    {
+        public TrainController(ICommandBus commandBus,
+                               IMenuRepository menuRepository,
+                               IPartRepository partRepository)
+            : base(commandBus, menuRepository, partRepository)
+        {
+        }
+
+        public ActionResult QA()
+        {
+            return base.DefaultResult();
+        }
+    }
+}
--- YiHe.Web/Scripts/baiduAnalytics.js
+++ YiHe.Web/Scripts/baiduAnalytics.js
@@ -1,2 +1,7 @@
-﻿var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fa41750d1db10729298185284b499d21b' type='text/javascript'%3E%3C/script%3E"));
+﻿var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?32b8d5e6ef46ca7713d4e3f6e3a661b7";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
